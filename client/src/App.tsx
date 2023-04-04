813930f12254655199b0a27830c75ec06411e060
Jeni <ziahrahl@gmail.com>
2023-04-03 20:47:03 -0400

    update styling files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,44 +2,41 @@ import React from 'react'
 import chameleonIcon from '/chameleon.svg'
 import searchIcon from '/search.svg'
 import personIcon from '/person.svg'
-import styles from './Header.module.scss'
-import {Container, Nav, Navbar } from 'react-bootstrap'
+import '../../scss/_globals.scss'
+import { Navbar, Container, Nav, Col, Button } from 'react-bootstrap'
+
 
 const Header = () => {
   return (
-      <header className={styles.header}>
-        <Navbar collapseOnSelect expand='lg' sticky='top'>
-        <Container>
-        <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={chameleonIcon}
-              width="40"
-              height="40"
-              className="d-inline-block align-top"
-            />{' '}
-            <span className={styles.brandText}>  Emerald Blog </span>
-          </Navbar.Brand>
-          
-          <div className={styles.iconContainer}>
-          <Nav.Link href='#' className={styles.createPostLink}>
-              Create Post
-            </Nav.Link>
-            <img src={searchIcon} alt="search-icon" width='25' height='25' />
-            <img src={personIcon} alt="person-icon" width='25' height='25' />
-          </div>
-
-        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
-          <Navbar.Collapse id="responsive-navbar-nav">
+      <Container>
+        <Navbar sticky='top' className=''>
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src={chameleonIcon}
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+              />
+            </Navbar.Brand>
+            <Col className='text-success'>
+            Emerald Blog
+            </Col>
             <Nav className="text-center ms-auto">
-              <Nav.Link href="#">Home</Nav.Link>
-              <Nav.Link href="#">Create Post</Nav.Link>
-              <Nav.Link href="#">Account</Nav.Link>
+            <Button type="button" className="btn btn-outline-secondary btn-light me-3">Create Post</Button>
+              <Nav.Link href="#">
+              <img src={searchIcon} alt="search-icon" width='25' height='25'  />
+              </Nav.Link>
+              
+              <Nav.Link href="#">
+              <img src={personIcon} alt="person-icon" width='30' height='30' />
+              </Nav.Link>
             </Nav>
-          </Navbar.Collapse>
+
         </Container>
       </Navbar>
-      </header>
+     </Container>
   )
 }
 
