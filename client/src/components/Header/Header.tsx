2688c2fd71cac5649ab3afc50b6dd49f7a215dc7
Jeni <ziahrahl@gmail.com>
2023-04-04 11:41:03 -0400

    hide/show elements based on screen size

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,39 +1,66 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 import chameleonIcon from '/chameleon.svg'
 import searchIcon from '/search.svg'
-import personIcon from '/person.svg'
+import personIcon from '/person-circle.svg'
 import '../../scss/_globals.scss'
-import { Navbar, Container, Nav, Col, Button } from 'react-bootstrap'
+import { Navbar, Container, Nav, Col, NavDropdown, Form, Button} from 'react-bootstrap'
 
 
 const Header = () => {
-  return (
-      <Container>
-        <Navbar sticky='top' className=''>
-          <Container>
-            <Navbar.Brand href="#home">
-              <img
-                alt=""
-                src={chameleonIcon}
-                width="40"
-                height="40"
-                className="d-inline-block align-top"
-              />
-            </Navbar.Brand>
-            <Col className='text-success'>
-            Emerald Blog
-            </Col>
-            <Nav className="text-center ms-auto">
-            <Button type="button" className="btn btn-outline-secondary btn-light me-3">Create Post</Button>
-              <Nav.Link href="#">
-              <img src={searchIcon} alt="search-icon" width='25' height='25'  />
-              </Nav.Link>
-              
-              <Nav.Link href="#">
-              <img src={personIcon} alt="person-icon" width='30' height='30' />
-              </Nav.Link>
-            </Nav>
 
+  const handleMobileSearchClick = () => {
+    console.log('do something when search icon is clicked in mobile view')
+    
+  }
+
+  const handleFormSubmit = () => {
+    console.log('do something when form is submitted from navbar on larger screen sizes')
+  }
+  
+  return (
+      <Container className='shadow' >
+        <Navbar sticky='top' >
+          <Container fluid>
+              <Navbar.Brand href="/" className='d-flex align-items-end'>
+                <img
+                  alt=""
+                  src={chameleonIcon}
+                  width="40"
+                  height="40"
+                  className="d-inline-block align-top me-3"
+                />
+                <h3 className='d-none d-md-inline-block text-primary font-weight-bolder mb-0'>Emerald Blog</h3>
+              </Navbar.Brand>
+      
+            <Nav className="text-center ms-auto d-flex align-items-center">
+              <Form className="d-none d-lg-flex" onSubmit={handleFormSubmit}>
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2 bg-white border border-secondary"
+                    aria-label="Search"
+                  />
+                  <Button variant="none" type="submit">
+                    <img src={searchIcon} alt="search-icon" width='25' height='25'  />
+                  </Button>
+              </Form>
+                <Nav.Item className='mx-2 d-lg-none' onClick={handleMobileSearchClick}>
+                  <img src={searchIcon} alt="search-icon" width='25' height='25'  />
+                </Nav.Item>
+                <Nav.Item className='ms-2 me-0'>
+                  <img src={personIcon} alt="search-icon" width='25' height='25'  />
+                </Nav.Item>
+                
+                <NavDropdown title='' id="nav-dropdown" align={"end"} className='custom-dropdown'>
+                  <NavDropdown.Item eventKey="4.1" className='text-primary'>USERNAME</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item eventKey="4.2" className='text-secondary'>Create Post</NavDropdown.Item>
+                  <NavDropdown.Item eventKey="4.3" className='text-secondary'>Profile</NavDropdown.Item>
+                  <NavDropdown.Item eventKey="4.4" className='text-secondary'>Favorites</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item eventKey="4.5">Log In/Out</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
         </Container>
       </Navbar>
      </Container>
