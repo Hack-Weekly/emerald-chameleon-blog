47be3173a65769c1b46207da4f814ecf5f50bf2c
Damanpreet Singh <daman0797@gmail.com>
2023-04-05 15:37:05 -0400

    fixed styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import LoginForm from './components/LoginForm'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CreatePost from './components/CreatePost'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
 
         <Routes>
           <Route path="/login" element={<LoginForm />} />
+          <Route path="/post" element={<CreatePost />} />
         </Routes>
 
       </Router>
--- client/src/components/CreatePost.tsx
+++ client/src/components/CreatePost.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { HttpTransportType, HubConnectionBuilder } from '@microsoft/signalr'
 import { ReadTokensFromLocalStorage } from '../services/authentication/authentication.service'
+import "../scss/styles.scss"
 import { Button, Container, Form } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
@@ -11,7 +11,7 @@ type CreateChatValues = {
   category: string,
 }
 
-export default function CreateNewChat() {
+const CreatePost = () => {
   const [route, setRoute] = useState('')
   const [formValues, setFormValues] = useState<CreateChatValues>({
     title: '',
@@ -32,26 +32,7 @@ export default function CreateNewChat() {
       e.preventDefault()
 
         try {
-        const connection = new HubConnectionBuilder()
-            .withUrl(process.env.NEXT_PUBLIC_API_URL, {
-            skipNegotiation: true,
-            transport: HttpTransportType.WebSockets,
-            accessTokenFactory: () => {
-                return ReadTokensFromLocalStorage().accessToken ?? ''
-            }
-            })
-            .build() 
-
-        await connection.start()
-        connection.invoke('CreatePost', formValues.title, formValues.description)
-        const connectionArr = Object.entries(connection)
-        connection.invoke('GetActivePosts')
-        connection.on('activePosts', (postsList) =>
-            postsList.unshift(connectionArr)
-        )
-        if (connectionArr[16][1] === 'Connected') {
-            setRoute('/dashboard')
-        }
+            console.log("post request")
         } catch (error) {
         alert('Failed to create new post, please try again.')
         setRoute('/dashboard')
@@ -60,72 +41,76 @@ export default function CreateNewChat() {
     }
 
     return (
-        <Container fluid>
-        <Container fluid>
-            <LinkContainer to="/dashboard">&#x25c4; Back</LinkContainer>
-            <h2>New Chat</h2>
-        </Container>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group>
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control 
-                    id="title"
-                    type="text"
-                    name="title"
-                    required
-                    value={formValues.title}
-                    onChange={handleInputChange}
-                    />
-                </Form.Group>
+        <Form onSubmit={handleSubmit} className='border p-3 rounded-5'>
+            <h2 className="text-center">New Chat</h2>
+            <Form.Group className="my-3">
+                <Form.Label>Title</Form.Label>
+                <Form.Control 
+                id="title"
+                type="text"
+                name="title"
+                required
+                value={formValues.title}
+                onChange={handleInputChange}
+                />
+            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Body</Form.Label>
-                    <Form.Control 
-                    id="body"
-                    type="text"
-                    name="body"
-                    required
-                    value={formValues.body}
-                    onChange={handleInputChange}
-                    />
-                </Form.Group>
+            <Form.Group className="my-3">
+                <Form.Label>Body</Form.Label>
+                <Form.Control 
+                id="body"
+                type="text"
+                name="body"
+                required
+                value={formValues.body}
+                onChange={handleInputChange}
+                />
+            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Tags</Form.Label>
-                    <Form.Control 
-                    id="tags"
-                    type="text"
-                    name="tags"
-                    value={formValues.tags}
-                    onChange={handleInputChange}
-                    />
-                </Form.Group>
+            <Form.Group className="my-3">
+                <Form.Label>Tags</Form.Label>
+                <Form.Control 
+                id="tags"
+                type="text"
+                name="tags"
+                value={formValues.tags}
+                onChange={handleInputChange}
+                />
+            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Category</Form.Label>
-                    <Form.Control 
-                    id="category"
-                    type="text"
-                    name="category"
-                    value={formValues.category}
-                    onChange={handleInputChange}
-                    />
-                </Form.Group>
+            <Form.Group className="my-3">
+                <Form.Label>Category</Form.Label>
+                <Form.Control 
+                id="category"
+                type="text"
+                name="category"
+                value={formValues.category}
+                onChange={handleInputChange}
+                />
+            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Image</Form.Label>
-                    <Form.Control 
-                    id="uploadImage"
-                    type="file"
-                    name="image"
-                    />
-                </Form.Group>
+            <Form.Group className="my-3">
+                <Form.Label>Image</Form.Label>
+                <Form.Control 
+                id="uploadImage"
+                type="file"
+                name="image"
+                />
+            </Form.Group>
 
-                <Container>
-                    <Button variant="primary" type="submit">Create Post</Button>
-                    <Button variant="outline-secondary">Cancel</Button>
-                </Container>
-            </Form>
-        </Container>
+            <Container className="my-3 text-center">
+                <Button className="mx-2" variant="primary" type="submit">
+                    Create Post
+                </Button>
+
+                <LinkContainer to="/dashboard">
+                        <Button variant="outline-secondary" className='mx-2'>
+                            Cancel
+                        </Button>
+                </LinkContainer>
+            </Container>
+        </Form>
     )
 }
+
+export default CreatePost
