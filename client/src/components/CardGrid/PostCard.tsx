aa9389520f40dc274586031535fa540a7da5e20a
codyhenry <codyhenry527@gmail.com>
2023-04-06 17:37:47 -0400

    Added button to postcard

--- client/src/components/CardGrid/PostCard.tsx
+++ client/src/components/CardGrid/PostCard.tsx
@@ -1,33 +1,35 @@
-import Card from 'react-bootstrap/Card';
-import { Post } from './CardGrid';
+import Card from 'react-bootstrap/Card'
+import { Post } from './CardGrid'
+import LikeButton from '../LikeBtn'
 import './cardGrid.scss'
 import '../../scss/styles.scss'
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
 import { Link } from 'react-router-dom'
 
 type PostCardProps = {
-  post: Post;
-  bgColor: string;
+  post: Post
+  bgColor: string
 }
 
-const PostCard= (props: PostCardProps) => {
-
+const PostCard = (props: PostCardProps) => {
   const cardBody = `${props.post.body.slice(0, 80)}...`
- 
+
   return (
-    <Link to={`/BlogPost/${props.post.id}`} className='text-decoration-none'>
-      <Card style={{ width: '15rem' }} className={`p-2 text-light h-100 ${props.bgColor}`}>
-        <Card.Img variant="top" src="/testImage1.png" height='125'/>
-        <Card.Body className='p-1 mt-1'>
+    <Link to={`/BlogPost/${props.post.id}`} className="text-decoration-none">
+      <Card
+        style={{ width: '15rem' }}
+        className={`p-2 text-light h-100 ${props.bgColor}`}
+      >
+        <Card.Img variant="top" src="/testImage1.png" height="125" />
+        <Card.Body className="p-1 mt-1">
           <Card.Title>{props.post.title}</Card.Title>
-          <Card.Subtitle className='fs-6 my-2'>MM/DD/YYYY</Card.Subtitle>
-          <Card.Text className='fw-normal' >
-            {cardBody}
-          </Card.Text>
+          <Card.Subtitle className="fs-6 my-2">MM/DD/YYYY</Card.Subtitle>
+          <Card.Text className="fw-normal">{cardBody}</Card.Text>
+          <LikeButton />
         </Card.Body>
       </Card>
     </Link>
-  ) 
+  )
 }
 
 export default PostCard
