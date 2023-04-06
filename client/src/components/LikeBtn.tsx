5c4e91cc21da8311685195a49314a0e102c9fa7c
codyhenry <codyhenry527@gmail.com>
2023-04-06 17:22:52 -0400

    Added icon to button and aligned numbers

--- client/src/components/LikeBtn.tsx
+++ client/src/components/LikeBtn.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import ToggleButton from 'react-bootstrap/ToggleButton'
 
+import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa'
+
 export default function LikeButton() {
   const [liked, setLiked] = useState(false)
   const [disliked, setDisliked] = useState(false)
@@ -38,28 +40,47 @@ export default function LikeButton() {
   return (
     <>
       <ButtonGroup className="mb-2">
-        <ToggleButton
-          id="toggle-likes"
-          type="checkbox"
-          variant="primary"
-          checked={liked}
-          value="1"
-          onChange={() => handleLike()}
-        >
+        <span>
+          <ToggleButton
+            id="toggle-likes"
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              paddingRight: 1,
+              position: 'relative',
+              top: -6,
+            }}
+            type="checkbox"
+            checked={liked}
+            value="1"
+            onChange={() => handleLike()}
+          >
+            <FaThumbsUp color="#b3ee74" />
+          </ToggleButton>
           {likes}
-        </ToggleButton>
+        </span>
       </ButtonGroup>
       <ButtonGroup className="mb-2">
-        <ToggleButton
-          id="toggle-dislikes"
-          type="checkbox"
-          variant="secondary"
-          checked={disliked}
-          value="1"
-          onChange={() => handleDislike()}
-        >
+        <span>
+          <ToggleButton
+            id="toggle-dislikes"
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              paddingRight: 1,
+              position: 'relative',
+              top: -1,
+            }}
+            type="checkbox"
+            variant="secondary"
+            checked={disliked}
+            value="1"
+            onChange={() => handleDislike()}
+          >
+            <FaThumbsDown color="#b3ee74" />
+          </ToggleButton>
           {dislikes}
-        </ToggleButton>
+        </span>
       </ButtonGroup>
     </>
   )
